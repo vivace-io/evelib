279532389c4fd32707f26f769fdda2c46168ae26
Vivace <vivace.naaris@gmail.com>
2016-08-09 23:18:10 -0400

    support for `alliances/` endpoint

--- crest/alliance.go
+++ crest/alliance.go
@@ -1,8 +1,25 @@
 package crest
 
+import "fmt"
+
+func GetAlliance(id int) (result *Alliance, err error) {
+	err = fetch(fmt.Sprintf("alliances/%v/", id), &result)
+	return
+}
+
 // Alliance in Eve
 type Alliance struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Href string `json:"href"`
+	ID                  int            `json:"id"`
+	Name                string         `json:"name"`
+	ShortName           string         `json:"shortName"`
+	Href                string         `json:"href"`
+	StartDateStr        string         `json:"startDate"`
+	Description         string         `json:"description"`
+	CorporationsCount   int            `json:"corporationsCount"`
+	ExecutorCorporation *Corporation   `json:"executorCorporation"`
+	Deleted             bool           `json:"deleted"`
+	CreatorCorporation  *Corporation   `json:"creatorCorporation"`
+	URL                 string         `json:"url"`
+	CreatorCharacter    *Character     `json:"creatorCharacter"`
+	Corporations        []*Corporation `json:"corporations"`
 }
---
+++ crest/alliance_test.go
@@ -0,0 +1,22 @@
+package crest
+
+import "testing"
+
+func TestGetAlliance(t *testing.T) {
+	t.Parallel()
+	// Wrong Hole. [99006213]
+	// https://crest-tq.eveonline.com/alliances/99006213/
+	result, err := GetAlliance(99006213)
+	if err != nil {
+		t.Error(err)
+	}
+	if result.ID != 99006213 {
+		t.Errorf("alliance ID mismatch - expected 99006213 but got", result.ID)
+	}
+	if result.Name != "Wrong Hole." {
+		t.Errorf("alliance name mismatch - expected \"Wrong Hole\" but got \"%v\"", result.Name)
+	}
+	if len(result.Corporations) != result.CorporationsCount {
+		t.Errorf("member corporation mismatch - %v member corporations listed, with %v counted for", len(result.Corporations), result.CorporationsCount)
+	}
+}
