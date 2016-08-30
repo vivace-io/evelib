0cbc90f3d6a6603339f71c766e9260982d4cf0f2
Vivace <vivace.naaris@gmail.com>
2016-08-30 16:58:03 -0400

    removed old code

--- zkill/redisq.go
+++ zkill/redisq.go
@@ -22,22 +22,6 @@ const ZKillboardRedisQ = "https://redisq.zkillboard.com/listen.php"
 
 var alreadyRunning = false
 
-// RedisQClient is a client to zKillboard's RedisQ service
-type RedisQClient struct {
-	RedisQURI string
-	webClient http.Client
-	UserAgent string
-}
-
-// NewRedisQ returns a client with default endpoints
-func NewRedisQ() *RedisQClient {
-	c := &RedisQClient{
-		RedisQURI: ZKillboardRedisQ,
-		webClient: http.Client{},
-	}
-	return c
-}
-
 type redisqResp struct {
 	Kill Kill `json:"package"`
 }
@@ -85,25 +69,6 @@ func RedisQAddChannel(output chan Kill) {
 
 func RedisQAddReciever(reciever RedisQReciever) {
 	redisQRecievers = append(redisQRecievers, reciever)
-}
-
-// FetchKillmails starts retrieving Killmails from ZKillboard RedisQ, sending
-// them (and any errors encountered) through the channels passed
-func (c *RedisQClient) FetchKillmails(output chan Kill, errChan chan error) {
-	if c.UserAgent == "" {
-		errChan <- errors.New("user-agent must be set first")
-		return
-	}
-	go func() {
-		for {
-			kill, err := fetchRedisQ()
-			if err != nil {
-				errChan <- err
-			} else {
-				output <- kill
-			}
-		}
-	}()
 }
 
 func fetchRedisQ() (k Kill, err error) {
