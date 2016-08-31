8a7e88134da83d4c0701d3d8c71b85cf4d4485a6
Vivace <vivace.naaris@gmail.com>
2016-08-31 14:45:50 -0400

    zkill client is back, documentation added

--- zkill/zkill.go
+++ zkill/zkill.go
@@ -10,7 +10,7 @@ import (
 // ZKillboardAPIURI is the default URL to zKillboard API
 const (
 	// DefaultAPIAddr for zKillboard
-	DefaultAPIAddr = "https://zkillboard.com/api/"
+	DefaultAPIAddr = "https://zkillboard.com/api"
 	// DefaultRedisQURI for zKillboard's RedisQ service
 	DefaultRedisQURI = "https://redisq.zkillboard.com/listen.php"
 )
@@ -23,30 +23,50 @@ var (
 	webClient  *http.Client
 )
 
-func Init(userAgent string) error {
-	userAgent = UserAgent
-	APIAddr = DefaultAPIAddr
-	RedisQAddr = DefaultRedisQURI
-	webClient = new(http.Client)
-	// return checkUserAgent()
-	return nil
+// Client is a client to access zKillboard's API.
+type Client struct {
+	UserAgent string
+	Server    string
+	clear     chan bool
 }
 
+func NewClient(address, userAgent string) (client *Client) {
+	client = &Client{
+		UserAgent: userAgent,
+		Server:    address,
+		clear:     make(chan bool, 100),
+	}
+	client.manage()
+	return
+}
+
+func (this *Client) manage() {
+	go func() {
+		for {
+			this.clear <- true
+			time.Sleep(10 * time.Millisecond)
+		}
+	}()
+}
+
+// Options is passed to query functions (i.e. CharacterKills, CorporationKills)
+// and modifies the scope of the request and the kills returned.
+// Options is passed to the client by design, as it is not required.
 type Options struct {
-	BeforeKillID int
-	AfterKillID  int
-	Solo         bool
-	Kills        bool
-	Losses       bool
-	WSpace       bool
-	Limit        int
+	BeforeKillID int  // Returns kills before the kill ID, if set.
+	AfterKillID  int  // Returns kills after the kill ID, if set.
+	Solo         bool // Only returns solo kills if true.
+	Kills        bool // Only returns kills if true.
+	Losses       bool // Only returns losses if true.
+	WSpace       bool // Only returns w-space kills if true.
+	Limit        int  // Maximum kills returned (default 200 if not set)
 }
 
 // Kill is contains the CREST Killmail and zKillboard's extra data.
 type Kill struct {
-	KillID   int             `json:"killID"`
-	Zkb      *Zkb            `json:"zkb"`
-	Killmail *crest.Killmail `json:"killmail"`
+	KillID   int            `json:"killID"`
+	Zkb      Zkb            `json:"zkb"`
+	Killmail crest.Killmail `json:"killmail"`
 }
 
 // Zkb is the extra data returned from zKillboard's API
@@ -60,14 +80,4 @@ type Zkb struct {
 
 type response struct {
 	Kills []Kill
-}
-
-func manage() {
-	clear = make(chan bool, 100)
-	go func() {
-		for {
-			clear <- true
-			time.Sleep(10 * time.Millisecond)
-		}
-	}()
 }
