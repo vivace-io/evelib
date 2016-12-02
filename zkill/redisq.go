00d5c79ce68d34d71dda02897819a4cf12200054
Vivace <vivace.naaris@gmail.com>
2016-12-02 01:54:48 -0500

    improved asynchronous saftey (read: it exists now) and expanded documentation

--- zkill/redisq.go
+++ zkill/redisq.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -16,18 +17,43 @@ type redisqResp struct {
 
 type RedisQClient struct {
 	UserAgent string
+	locker    *sync.RWMutex
 	errc      chan error
 	recievers []RedisQReciever
 	channels  []chan Kill
 	running   bool
 }
 
+// NewRedisQClient returns a new client to ZKillboard's RedisQ service.
 func NewRedisQClient(userAgent string) (client *RedisQClient) {
 	client = new(RedisQClient)
 	client.UserAgent = userAgent
+	client.locker = new(sync.RWMutex)
 	return
 }
 
+// AddChannel registers a channel to be sent all future kills from RedisQ. Bear
+// in mind that the RedisQClient will start a new goroutine for each channel
+// added when pushing the Kill to the channel, so if the channel is not being
+// emptied fast enough or is in deadlock, the program implementing this client
+// will likely crash and burn. Safe for asynchronous use.
+func (this *RedisQClient) AddChannel(output chan Kill) {
+	this.locker.Lock()
+	defer this.locker.Unlock()
+	this.channels = append(this.channels, output)
+}
+
+// AddReciever registers a function of type RedisQReciever to be sent all future
+// kills from RedisQ. Bear in mind that the clien will send the kill in a
+// seperate goroutine for each reciever, so if the funcion takes too long/hangs
+// the program will quickly become bloated and crash. Safe for asynchronous use.
+func (this *RedisQClient) AddReciever(reciever RedisQReciever) {
+	this.locker.Lock()
+	defer this.locker.Unlock()
+	this.recievers = append(this.recievers, reciever)
+}
+
+// Start listening to RedisQ. Once started, does not block.
 func (this *RedisQClient) Start() error {
 	if this.running {
 		return errors.New("already watching redisq")
@@ -49,15 +75,21 @@ func (this *RedisQClient) Start() error {
 	return nil
 }
 
+// SetErrorChannel takes a channel of any size and asynchronously sends it
+// all errors RedisQ experiences while running. Errors are sent on a seperate
+// goroutine, but take care that the channel never fills for too long.
 func (this *RedisQClient) SetErrorChannel(errChan chan error) {
 	this.errc = errChan
 }
 
+// Stop RedisQ from retrieving any more kills.
 func (this *RedisQClient) Stop() {
 	this.running = false
 }
 
 func (this *RedisQClient) send(k Kill) {
+	this.locker.RLock()
+	defer this.locker.RUnlock()
 	for _, c := range this.channels {
 		go func() {
 			c <- k
@@ -66,14 +98,6 @@ func (this *RedisQClient) send(k Kill) {
 	for _, r := range this.recievers {
 		go r(k)
 	}
-}
-
-func (this *RedisQClient) AddChannel(output chan Kill) {
-	this.channels = append(this.channels, output)
-}
-
-func (this *RedisQClient) AddReciever(reciever RedisQReciever) {
-	this.recievers = append(this.recievers, reciever)
 }
 
 func (this *RedisQClient) fetch() (k Kill, err error) {
