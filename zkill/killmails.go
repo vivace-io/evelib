f2176f2068dce46262f5b9d24c20b8b174718fda
Vivace <vivace.naaris@gmail.com>
2016-12-02 02:12:01 -0500

    Implemented zkill.Historical to pull historical killmail dumps for specified dates... could have sworn I already did this but here we are.

--- zkill/killmails.go
+++ zkill/killmails.go
@@ -1,10 +1,25 @@
 package zkill
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 // Historical returns all kill ID's and their accompanying hashes submittted to
 // zKillboard on the provided date.
 func (client *Client) Historical(date time.Time) (killmails map[int]string, err error) {
 	// TODO
+	results := make(map[string]string)
+	client.fetch(fmt.Sprintf("/history/%v/", date.Format("20060102")), &results)
+	var id int
+	for key, val := range results {
+		if id, err = strconv.Atoi(key); err != nil {
+			killmails[id] = val
+		} else {
+			err = fmt.Errorf("unable to conver string value '%v' to type int: %v", key, err)
+			return
+		}
+	}
 	return
 }
