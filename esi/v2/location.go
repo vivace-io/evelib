43a92edd402005de650d959346a8985a216230b6
Vivace <vivace.naaris@gmail.com>
2017-05-04 11:35:50 -0400

    ESI location documentation/general cleanup

--- esi/v2/location.go
+++ esi/v2/location.go
@@ -2,6 +2,62 @@ package esi
 
 import "fmt"
 
+// RegionIDs returns a list of constellation IDs from ESI.
+func (client *Client) RegionIDs() (results []int, err error) {
+	path := buildPath("/universe/regions/")
+	err = client.get(path, &results)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// RegionGet accepts a region's ID and returns its ESI model.
+func (client *Client) RegionGet(id int) (result *Region, err error) {
+	err = client.get(fmt.Sprintf("/universe/regions/%v/"), &result)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// Region in EVE Online.
+type Region struct {
+	ID             int    `json:"region_id"`
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	Constellations int    `json:"constellations"`
+}
+
+// ConstellationIDs returns a list of constellation IDs from ESI.
+func (client *Client) ConstellationIDs() (results []int, err error) {
+	path := buildPath("/universe/constellations/")
+	err = client.get(path, &results)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// ConstellationGet accepts a constellation's ID and returns its ESI model.
+func (client *Client) ConstellationGet(id int) (result *Constellation, err error) {
+	err = client.get(fmt.Sprintf("/universe/constellations/%v/"), &result)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// Constellation in EVE Online.
+type Constellation struct {
+	ID        int      `json:"constellation_id"`
+	Name      string   `json:"name"`
+	Position  Position `json:"position"`
+	RegionID  int      `json:"region_id"`
+	SystemIDs []int    `json:"systems"`
+}
+
+// SystemIDs returns a list of system IDs from ESI.
 func (client *Client) SystemIDs() (results []int, err error) {
 	path := buildPath("/universe/systems/")
 	err = client.get(path, &results)
@@ -11,6 +67,7 @@ func (client *Client) SystemIDs() (results []int, err error) {
 	return
 }
 
+// SystemGet accepts a solar system's ID and returns its ESI model.
 func (client *Client) SystemGet(id int) (result *System, err error) {
 	path := buildPath(fmt.Sprintf("/universe/systems/%v/", id))
 	err = client.get(path, &result)
@@ -27,62 +84,14 @@ type System struct {
 	SecurityClass  string   `json:"security_class"`
 	SecurityStatus float32  `json:"security_status"`
 	Position       Position `json:"position"`
+	StargateIDs    []int    `json:"stargates"`
 	// TODO
-	// Stargates []Stargate `json:"stargates"`
 	// Planets []Planet `json:"planets"`
 }
 
-func (client *Client) ConstellationIDs() (results []int, err error) {
-	path := buildPath("/universe/constellations/")
-	err = client.get(path, &results)
-	if err != nil {
-		return nil, err
-	}
-	return
-}
-
-func (client *Client) ConstellationGet(id int) (result *Constellation, err error) {
-	err = client.get(fmt.Sprintf("/universe/constellations/%v/"), &result)
-	if err != nil {
-		return nil, err
-	}
-	return
-}
-
-type Constellation struct {
-	ID       int      `json:"constellation_id"`
-	Name     string   `json:"name"`
-	Position Position `json:"position"`
-	RegionID int      `json:"region_id"`
-	Systems  []int    `json:"systems"`
-}
-
-func (client *Client) RegionIDs() (results []int, err error) {
-	path := buildPath("/universe/regions/")
-	err = client.get(path, &results)
-	if err != nil {
-		return nil, err
-	}
-	return
-}
-
-func (client *Client) RegionGet(id int) (result *Region, err error) {
-	err = client.get(fmt.Sprintf("/universe/regions/%v/"), &result)
-	if err != nil {
-		return nil, err
-	}
-	return
-}
-
-type Region struct {
-	ID             int    `json:"region_id"`
-	Name           string `json:"name"`
-	Description    string `json:"description"`
-	Constellations int    `json:"constellations"`
-}
-
+// Position in 3D space in EVE Online.
 type Position struct {
-	X float32 `json:"x"`
-	Y float32 `json:"y"`
-	Z float32 `json:"z"`
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+	Z float64 `json:"z"`
 }
