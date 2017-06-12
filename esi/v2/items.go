c79ec8898a908c3c00e56e497f5201d35c0cbabc
Vivace <morley@vivace.io>
2017-06-12 13:02:59 -0400

    implemented ItemGet for retrieving a specified Item Type from ESI

--- esi/v2/items.go
+++ esi/v2/items.go
@@ -2,6 +2,7 @@ package esi
 
 import "fmt"
 
+// ItemIDs returns a list of all Item Type IDs from ESI.
 func (client *Client) ItemIDs() (results []int, err error) {
 	for p := 1; ; p++ {
 		var ids []int
@@ -16,5 +17,28 @@ func (client *Client) ItemIDs() (results []int, err error) {
 			break
 		}
 	}
+	return
+}
+
+// Item is an item type from EVE Online.
+// TODO - Dogma Attributes/Effects
+type Item struct {
+	TypeID      int     `json:"type_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Published   bool    `json:"published"`
+	GroupID     int     `json:"group_id"`
+	Radius      float32 `json:"radius"`
+	Volume      float32 `json:"volume"`
+	Capacity    float32 `json:"capacity"`
+	PortionSize int     `json:"portion_size"`
+	Mass        float32 `json:"mass"`
+	IconID      int     `json:"icon_id"`
+}
+
+// ItemGet returns the ESI Item Type representation for the given ID.
+func (client *Client) ItemGet(id int) (result *Item, err error) {
+	path := buildPath(fmt.Sprintf("/universe/types/%v/", id))
+	err = client.get(path, &result)
 	return
 }
--- esi/v2/items_test.go
+++ esi/v2/items_test.go
@@ -12,3 +12,41 @@ func TestItemIDs(t *testing.T) {
 		t.Errorf("want 32013 IDs but have %v", len(ids))
 	}
 }
+
+// TestItemGetVeldspar retrieves the Veldspar type from the ESI types endpoint
+// and validates it's fields.
+func TestItemGet(t *testing.T) {
+	t.Parallel()
+	var err error
+	var veldspar *Item
+	if veldspar, err = testClient.ItemGet(1230); err != nil {
+		t.Errorf("failed to retrieve Item Type Veldspar: %v", err)
+		return
+	}
+	if veldspar == nil {
+		t.Error("veldspar was returned as nil")
+		return
+	}
+	if veldspar.TypeID != 1230 {
+		t.Errorf("Field TypeID mismatch - want '1230' but have '%v'", veldspar.TypeID)
+	}
+	if veldspar.Name != "Veldspar" {
+		t.Errorf("Field Name mismatch - want 'Veldspar' but have '%v'", veldspar.Name)
+	}
+	// Because checking against the entire text description is a little over the top...
+	if len(veldspar.Description) == 0 {
+		t.Error("Field Description was empty")
+	}
+	if veldspar.Published != true {
+		t.Error("Field Published mismatch - want 'true' but have 'false'")
+	}
+	if veldspar.GroupID != 462 {
+		t.Errorf("Field GroupID mismatch - want '462' but have '%v'", veldspar.GroupID)
+	}
+	if veldspar.Radius != 1 {
+		t.Errorf("Field Radius mismatch - want '1' but have '%v'", veldspar.Radius)
+	}
+	if veldspar.Volume != 0.1 {
+		t.Errorf("Field Volume mismatch - want '0.1' but have '%.2f'", veldspar.Radius)
+	}
+}
