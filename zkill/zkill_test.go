fb36dc56dda85e31ede61798f883077c5d12b180
Vivace <vivace.naaris@gmail.com>
2017-09-27 16:00:32 -0400

    zkill pkg cleanup

--- zkill/zkill_test.go
+++ zkill/zkill_test.go
@@ -13,17 +13,10 @@ var testClient *Client
  * Main entry point for package testing, sets up fixture(s) for testing.      *
  * ****************************************************************************/
 func TestMain(m *testing.M) {
-	opts := DefaultOptions()
-	// Disable TLS verification for Continuous Integration testing.
-	opts.DisableTLS = true
-	opts.UserAgent = "EveLib Testing Agent"
-	opts.MaxConn = 5
-	opts.RateLimit = 10
-	opts.RateBurst = 10
 	var err error
-	testClient, err = NewClient(opts)
-	if err != nil {
+	if testClient, err = NewClient(DefaultAddress, "EVELib Testing Agent <github.com/vivace-io/evelib>"); err != nil {
 		fmt.Printf("Failed to create test client: %v", err)
+		os.Exit(1)
 	}
 	os.Exit(m.Run())
 }
